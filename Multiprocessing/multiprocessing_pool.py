4b884362f1ced71671f88ad839965a381bb96107
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-06-02 11:58:08 +0530

    Update multiprocessing_pool.py

--- Multiprocessing/multiprocessing_pool.py
+++ Multiprocessing/multiprocessing_pool.py
@@ -1,11 +1,11 @@
 from multiprocessing import Pool
-
-
 def f(n):
     return n*n
 
 if __name__ == "__main__":
     p = Pool(processes=3)
-    result = p.map(f,[1,2,3,4,5])
+    result = p.map(f,[1,2,3,4,5]) #map means the process of dividing inputs/work equally  into the multiple cores
     for n in result:
         print(n)
+
+#reduce means collecting all the results from different cores
